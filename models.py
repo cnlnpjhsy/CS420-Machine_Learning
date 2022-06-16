fcf17f9001378aeb3822d9734d7868b108032a23
cnlnpjhsy <61415268+cnlnpjhsy@users.noreply.github.com>
2022-06-16 17:07:28 +0800

    baselineRNN added

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn_utils
 import numpy as np
 
 
@@ -32,4 +34,27 @@ class baselineCNN(nn.Module):
         x = self.conv(x)
         x = x.view(x.size()[0], -1) # Flatten
         y = self.fc(x)
+        return y
+
+
+class baselineRNN(nn.Module):
+    def __init__(self, input_size: int, n_labels: int, hidden_size=256) -> None:
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
+        self.linear1 = nn.Linear(hidden_size * 2, 512)
+        self.linear2 = nn.Linear(512, 256)
+        self.clf = nn.Linear(256, n_labels)
+
+    def forward(self, batch_x, batch_x_len):
+        packed_input = rnn_utils.pack_padded_sequence(
+            batch_x, batch_x_len, batch_first=True, enforce_sorted=False
+        )
+        packed_output, (hx, cx) = self.lstm(packed_input)   # hx: (2, batch_size, hidden_size)
+        hx_L = hx[-2]
+        hx_R = hx[-1]
+        x = torch.cat((hx_L, hx_R), dim=1)  # x: (batch_size, hidden_size * 2)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        y = self.clf(x)
+
         return y
