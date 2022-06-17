0b2c3f4af0c458dcbfdb927de5b4cd07de3b2f9c
cnlnpjhsy <61415268+cnlnpjhsy@users.noreply.github.com>
2022-06-17 14:06:06 +0800

    SimpleTwoBranch added

--- data_processor.py
+++ data_processor.py
@@ -18,7 +18,7 @@ class PNGDataset(Dataset):
         self.label_name = []
         for file in os.listdir(data_path):
             category_name = file[:-8]   # category_name_png.npz
-            data = np.load(os.path.join(data_path, file))[mode] # (len, 28, 28)
+            data = np.load(os.path.join(data_path, file))[mode] # (size, 28, 28)
             label_no = int(label_dict[category_name])
             label = np.ones(np.shape(data)[0], dtype=int) * label_no
             X.append(data)
@@ -46,7 +46,7 @@ class RAWDataset(Dataset):
         self.label_name = []
         for file in os.listdir(data_path):
             category_name = file[:-4]   # category_name.npz
-            data = np.load(os.path.join(data_path, file), encoding='latin1', allow_pickle=True)[mode]   # (len, 3)
+            data = np.load(os.path.join(data_path, file), encoding='latin1', allow_pickle=True)[mode]   # (size, )
             label_no = int(label_dict[category_name])
             label = np.ones(np.shape(data)[0], dtype=int) * label_no
             X.append(data)
@@ -65,3 +65,36 @@ class RAWDataset(Dataset):
         tensor_y = torch.tensor(self.y[idx]).long()     # (, )
         return tensor_x, tensor_y
         
+
+class TwoBranchDataset(Dataset):
+    def __init__(self, mode: str, data_path_raw: str, data_path_png: str) -> None:
+        super().__init__()
+        assert mode in ['train', 'valid', 'test']
+        X_raw, X_png, y = [], [], []
+        self.label_name = []
+        for file_raw, file_png in zip(os.listdir(data_path_raw), os.listdir(data_path_png)):
+            category_name_raw = file_raw[:-4]   # category_name.npz
+            category_name_png = file_png[:-8]   # category_name_png.npz
+            assert category_name_raw == category_name_png
+            data_raw = np.load(os.path.join(data_path_raw, file_raw), encoding='latin1', allow_pickle=True)[mode]   # (size, )
+            data_png = np.load(os.path.join(data_path_png, file_png))[mode]     # (size, 28, 28)
+            label_no = int(label_dict[category_name_raw])
+            label = np.ones(np.shape(data_raw)[0], dtype=int) * label_no
+            X_raw.append(data_raw)
+            X_png.append(data_png)
+            y.append(label)
+            self.label_name.append(category_name_raw)
+
+        self.X_raw = np.concatenate(X_raw)
+        self.X_png = np.concatenate(X_png)
+        self.y = np.concatenate(y)
+        print('Loaded %d samples from %s dataset.' % (self.__len__(), mode))
+
+    def __len__(self):
+        return np.shape(self.X_raw)[0]
+
+    def __getitem__(self, idx):
+        tensor_x_raw = torch.tensor(self.X_raw[idx]).float()    # (len, 3, )
+        tensor_x_png = torch.tensor(self.X_png[idx]).float().unsqueeze(0)   # (28, 28)
+        tensor_y = torch.tensor(self.y[idx]).long()     # (, )
+        return tensor_x_raw, tensor_x_png, tensor_y
--- models.py
+++ models.py
@@ -58,3 +58,47 @@ class baselineRNN(nn.Module):
         y = self.clf(x)
 
         return y
+
+
+class SimpleTwoBranch(nn.Module):
+    def __init__(self, input_size: int, image_shape: tuple, n_labels: int, hidden_size=256) -> None:
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
+        self.conv = nn.Sequential(
+            nn.Conv2d(image_shape[0], 20, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(20, 50, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        conv_out_size = self._get_conv_out(image_shape)
+        lstm_out_size = hidden_size * 2
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size + lstm_out_size, 500),
+            nn.ReLU(),
+            nn.Linear(500, 250),
+            nn.ReLU(),
+            nn.Linear(250, n_labels)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, seq_x, img_x, seq_x_len):
+        packed_input = rnn_utils.pack_padded_sequence(
+            seq_x, seq_x_len, batch_first=True, enforce_sorted=False
+        )
+        packed_output, (hx, cx) = self.lstm(packed_input)
+        hx_L = hx[-2]
+        hx_R = hx[-1]
+        seq_x = torch.cat((hx_L, hx_R), dim=1)
+
+        img_x = self.conv(img_x / 255.0)
+        img_x = img_x.view(img_x.size()[0], -1) # Flatten
+
+        x = torch.cat([seq_x, img_x], dim=1)
+        y = self.fc(x)
+
+        return y
